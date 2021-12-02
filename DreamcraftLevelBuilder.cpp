08750973514ba47999678e57b3dc43298b3f4d46
GreenThunder245 <82413608+GreenThunder245@users.noreply.github.com>
2021-12-02 12:21:31 -0500

    Update DreamcraftLevelBuilder.cpp

--- DreamcraftLevelBuilder.cpp
+++ DreamcraftLevelBuilder.cpp
@@ -1,17 +1,18 @@
 #include "raylib.h"
 #include <cmath>
 #include "player.h"
+#include <iostream>
+#include <fstream>
 
 
 
-
-
+using namespace std;
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 1280;
-    const int screenHeight = 800;
+    const int screenHeight = 720;
 
     //Initalize Audio Device
     InitAudioDevice();
@@ -22,12 +23,9 @@ int main(void)
     
 
 
-    //int levelDat[20][10];
-    int levelData[40][22] = {0
-    };
+    int levelData[40][20] = {0};
     Vector2 mousePosition;
     int mouseBlock = 0;
-    bool guiDrawn = false;
     //initalize Textures
     Texture2D playerTexture = LoadTexture("res/gfx/dream.png");
     Texture2D dirtTexture = LoadTexture("res/gfx/block/dirt.png");
@@ -42,7 +40,21 @@ int main(void)
     Texture2D birchWoodTexture = LoadTexture("res/gfx/block/birch_log.png"); 
     Texture2D birchLeavesTexture = LoadTexture("res/gfx/block/birch_leaves.png");
     Texture2D brickTexture = LoadTexture("res/gfx/block/bricks.png");
-
+    //Initalize Texture Pointers to pass as parameters to the varBlock Function 
+    Texture2D* dirtTexturePtr = &dirtTexture;
+    Texture2D* grassTexturePtr = &grassTexture; 
+    Texture2D* dirtTexturePtr = &oakLogTexture;
+    Texture2D* brickTexturePtr = &stoneTexture; 
+    Texture2D* dirtTexturePtr = &ironOreTexture;
+    Texture2D* brickTexturePtr = &coalOreTexture; 
+    Texture2D* dirtTexturePtr = &cobblestoneTexture;
+    Texture2D* dirtTexturePtr = &oakPlankTexture;
+    Texture2D* grassTexturePtr = &oakLeavesTexture; 
+    Texture2D* dirtTexturePtr = &birchWoodTexture;
+    Texture2D* brickTexturePtr = &birchLeavesTexture; 
+    Texture2D* brickTexturePtr = &brickTexture;
+    
+        
     //Initalize Sounds
 Sound breakBlock = LoadSound("res/sfx/sounds/random/break.ogg");
     //Initalize Music
@@ -50,7 +62,7 @@ Sound breakBlock = LoadSound("res/sfx/sounds/random/break.ogg");
     int mouseArray[2] = {0,0};
 
     //Init Player
-    Player dream = {100,100,false,false,false,false,0,0,0};
+    //Player dream = {100,100,false,false,false,false,0,0,0};
 
    
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -63,27 +75,18 @@ Sound breakBlock = LoadSound("res/sfx/sounds/random/break.ogg");
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         mousePosition = GetMousePosition();
-        if (mousePosition.y > 96)
+        if (mousePosition.y > 80)
         {
         mouseArray[0] = floor(mousePosition.x/32);
-        mouseArray[1] = floor((mousePosition.y-96)/32);
+        mouseArray[1] = floor((mousePosition.y-80)/32);
         if (IsMouseButtonPressed(0)){
         levelData[mouseArray[0]][mouseArray[1]] = mouseBlock;
         PlaySound(breakBlock);
-        switch (mouseBlock)
+        }
+        }
+        if (mousePosition.y < 80)
         {
-        case 0:
-                
-                break;
-        
-        default:
-                break;
-        }
-        }
-        }
-        if (mousePosition.y < 96)
-        {
-        if (IsMouseButtonPressed(0)) mouseBlock = (round(mousePosition.x/52)*2+floor(mousePosition.y/48)-2);
+        if (IsMouseButtonPressed(0)) mouseBlock = ((floor(mousePosition.x/52)*2)+(floor(mousePosition.y/40)));
         }
         //----------------------------------------------------------------------------------
 
@@ -96,160 +99,109 @@ Sound breakBlock = LoadSound("res/sfx/sounds/random/break.ogg");
             //draws the level
             for (int i = 0; i <40; i++)
             {
-                for (int d = 0; d < 22; d++)
+                for (int d = 0; d < 20; d++)
                 {
-                        //selects which texture to draw based on the integer
+                        //selects which texture to draw based on the integers stored in the level data
                         switch (levelData[i][d])
                         {
-                            case 0:
+                        case 0:
                                 break;
-                            case 1:
-                                DrawTexture(playerTexture,i*32,d*32+96,RAYWHITE);
+                        case 1:
+                                DrawTexture(dirtTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 2:
-                                DrawTexture(dirtTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(grassTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 3:
-                                DrawTexture(grassTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(oakLogTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 4:
-                                DrawTexture(oakLogTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(stoneTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 5:
-                                DrawTexture(stoneTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(ironOreTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 6:
-                                DrawTexture(ironOreTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(coalOreTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 7:
-                                DrawTexture(coalOreTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(cobblestoneTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 8:
-                                DrawTexture(cobblestoneTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(oakPlankTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 9:
-                                DrawTexture(oakPlankTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(oakLeavesTexture,i*32,d*32+80,DARKGREEN);
                                 break;
                         case 10:
-                                DrawTexture(oakLeavesTexture,i*32,d*32+96,DARKGREEN);
+                                DrawTexture(birchLeavesTexture,i*32,d*32+80,DARKGREEN);
                                 break;
                         case 11:
-                                DrawTexture(birchWoodTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(birchWoodTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 12:
-                            DrawTexture(birchLeavesTexture,i*32,d*32+96,WHITE);
+                                DrawTexture(brickTexture,i*32,d*32+80,WHITE);
                                 break;
                         case 13:
-                            DrawTexture(brickTexture,i*32,d*32+96,WHITE);
-                        case 14:
-                            break;
+
+                                break;
                         default:
-                            break;
+                                break;
                         }
                     }
                 }
 
-    //Block Selection GUI
-    while ((guiDrawn = false))
-    {
-        DrawRectangle(20,10,32,32,GRAY);
-        DrawRectangle(20,52,32,32,GREEN);
-        DrawTexture(dirtTexture,72,10,WHITE);
-        DrawTexture(grassTexture,72,52,WHITE);
-        DrawTexture(oakLogTexture,124,10,WHITE);
-        DrawTexture(stoneTexture,124,52,WHITE);
-        DrawTexture(ironOreTexture,176,10,WHITE);
-        DrawTexture(coalOreTexture,176,52,WHITE);
-        DrawTexture(cobblestoneTexture,228,10,WHITE);
-        DrawTexture(oakPlankTexture,228,52,WHITE);
-        DrawTexture(oakLeavesTexture,280,10,WHITE);
-        DrawTexture(birchLeavesTexture,280,52,DARKGREEN);
-        DrawTexture(birchWoodTexture,332,10,WHITE);
-        DrawTexture(brickTexture,332,52,WHITE);
-        DrawRectangle(384,10,32,32,BLUE);
-        DrawRectangle(384,52,32,32,BLUE);
-        DrawRectangle(436,10,32,32,BLUE);
-        DrawRectangle(436,52,32,32,BLUE);
-        DrawRectangle(488,10,32,32,BLUE);
-        DrawRectangle(488,52,32,32,BLUE);
-        DrawRectangle(540,10,32,32,BLUE);
-        DrawRectangle(540,52,32,32,BLUE);
-        DrawRectangle(592,10,32,32,BLUE);
-        DrawRectangle(592,52,32,32,BLUE);
-        DrawRectangle(644,10,32,32,BLUE);
-        DrawRectangle(644,52,32,32,BLUE);
-        DrawRectangle(696,10,32,32,BLUE);
-        DrawRectangle(696,52,32,32,BLUE);
-        DrawRectangle(748,10,32,32,BLUE);
-        DrawRectangle(748,52,32,32,BLUE);
-        DrawRectangle(800,10,32,32,BLUE);
-        DrawRectangle(800,52,32,32,BLUE);
-        DrawRectangle(852,10,32,32,BLUE);
-        DrawRectangle(852,52,32,32,BLUE);
-        DrawRectangle(904,10,32,32,BLUE);
-        DrawRectangle(904,52,32,32,BLUE);
-        DrawRectangle(956,10,32,32,BLUE);
-        DrawRectangle(956,52,32,32,BLUE);
-        DrawRectangle(1008,10,32,32,BLUE);
-        DrawRectangle(1008,52,32,32,BLUE);
-        DrawRectangle(1060,10,32,32,BLUE);
-        DrawRectangle(1060,52,32,32,BLUE);
-        DrawRectangle(1112,10,32,32,BLUE);
-        DrawRectangle(1112,52,32,32,BLUE);
-        DrawRectangle(1164,10,32,32,BLUE);
-        DrawRectangle(1164,52,32,32,BLUE);
-        DrawRectangle(1216,10,32,32,BLUE);
-        DrawRectangle(1216,52,32,32,BLUE);//Block Selection GUI Drawing
-        DrawRectangle(20,10,32,32,GRAY);
-        DrawRectangle(20,52,32,32,GREEN);
-        DrawTexture(dirtTexture,72,10,WHITE);
-        DrawTexture(grassTexture,72,52,WHITE);
-        DrawTexture(oakLogTexture,124,10,WHITE);
-        DrawTexture(stoneTexture,124,52,WHITE);
-        DrawTexture(ironOreTexture,176,10,WHITE);
-        DrawTexture(coalOreTexture,176,52,WHITE);
-        DrawTexture(cobblestoneTexture,228,10,WHITE);
-        DrawTexture(oakPlankTexture,228,52,WHITE);
-        DrawTexture(oakLeavesTexture,280,10,WHITE);
-        DrawTexture(birchLeavesTexture,280,52,DARKGREEN);
-        DrawTexture(birchWoodTexture,332,10,WHITE);
-        DrawTexture(brickTexture,332,52,WHITE);
-        DrawRectangle(384,10,32,32,BLUE);
-        DrawRectangle(384,52,32,32,BLUE);
-        DrawRectangle(436,10,32,32,BLUE);
-        DrawRectangle(436,52,32,32,BLUE);
-        DrawRectangle(488,10,32,32,BLUE);
-        DrawRectangle(488,52,32,32,BLUE);
-        DrawRectangle(540,10,32,32,BLUE);
-        DrawRectangle(540,52,32,32,BLUE);
-        DrawRectangle(592,10,32,32,BLUE);
-        DrawRectangle(592,52,32,32,BLUE);
-        DrawRectangle(644,10,32,32,BLUE);
-        DrawRectangle(644,52,32,32,BLUE);
-        DrawRectangle(696,10,32,32,BLUE);
-        DrawRectangle(696,52,32,32,BLUE);
-        DrawRectangle(748,10,32,32,BLUE);
-        DrawRectangle(748,52,32,32,BLUE);
-        DrawRectangle(800,10,32,32,BLUE);
-        DrawRectangle(800,52,32,32,BLUE);
-        DrawRectangle(852,10,32,32,BLUE);
-        DrawRectangle(852,52,32,32,BLUE);
-        DrawRectangle(904,10,32,32,BLUE);
-        DrawRectangle(904,52,32,32,BLUE);
-        DrawRectangle(956,10,32,32,BLUE);
-        DrawRectangle(956,52,32,32,BLUE);
-        DrawRectangle(1008,10,32,32,BLUE);
-        DrawRectangle(1008,52,32,32,BLUE);
-        DrawRectangle(1060,10,32,32,BLUE);
-        DrawRectangle(1060,52,32,32,BLUE);
-        DrawRectangle(1112,10,32,32,BLUE);
-        DrawRectangle(1112,52,32,32,BLUE);
-        DrawRectangle(1164,10,32,32,BLUE);
-        DrawRectangle(1164,52,32,32,BLUE);
-        DrawRectangle(1216,10,32,32,BLUE);
-        DrawRectangle(1216,52,32,32,BLUE);
-        guiDrawn = true;
-    }
+        
+        DrawRectangle(20,5,32,32,GRAY);
+        DrawTexture(dirtTexture,20,45,WHITE);
+        DrawTexture(grassTexture,72,5,WHITE);
+        DrawTexture(oakLogTexture,72,45,WHITE);
+        DrawTexture(stoneTexture,124,5,WHITE);
+        DrawTexture(ironOreTexture,124,45,WHITE);
+        DrawTexture(coalOreTexture,176,5,WHITE);
+        DrawTexture(cobblestoneTexture,176,45,WHITE);
+        DrawTexture(oakPlankTexture,228,5,WHITE);
+        DrawTexture(oakLeavesTexture,228,45,DARKGREEN);
+        DrawTexture(birchLeavesTexture,280,5,DARKGREEN);
+        DrawTexture(birchWoodTexture,280,45,WHITE);
+        DrawTexture(brickTexture,332,5,WHITE);
+        DrawRectangle(332,45,32,32,BLUE);
+        DrawRectangle(384,5,32,32,BLUE);
+        DrawRectangle(384,45,32,32,BLUE);
+        DrawRectangle(436,5,32,32,BLUE);
+        DrawRectangle(436,45,32,32,BLUE);
+        DrawRectangle(488,5,32,32,BLUE);
+        DrawRectangle(488,45,32,32,BLUE);
+        DrawRectangle(540,5,32,32,BLUE);
+        DrawRectangle(540,45,32,32,BLUE);
+        DrawRectangle(592,5,32,32,BLUE);
+        DrawRectangle(592,45,32,32,BLUE);
+        DrawRectangle(644,5,32,32,BLUE);
+        DrawRectangle(644,45,32,32,BLUE);
+        DrawRectangle(696,5,32,32,BLUE);
+        DrawRectangle(696,45,32,32,BLUE);
+        DrawRectangle(748,5,32,32,BLUE);
+        DrawRectangle(748,45,32,32,BLUE);
+        DrawRectangle(800,5,32,32,BLUE);
+        DrawRectangle(800,45,32,32,BLUE);
+        DrawRectangle(852,5,32,32,BLUE);
+        DrawRectangle(852,45,32,32,BLUE);
+        DrawRectangle(904,5,32,32,BLUE);
+        DrawRectangle(904,45,32,32,BLUE);
+        DrawRectangle(956,5,32,32,BLUE);
+        DrawRectangle(956,45,32,32,BLUE);
+        DrawRectangle(1008,5,32,32,BLUE);
+        DrawRectangle(1008,45,32,32,BLUE);
+        DrawRectangle(1060,5,32,32,BLUE);
+        DrawRectangle(1060,45,32,32,BLUE);
+        DrawRectangle(1112,5,32,32,BLUE);
+        DrawRectangle(1112,45,32,32,BLUE);
+        DrawRectangle(1164,5,32,32,BLUE);
+        DrawRectangle(1164,45,32,32,BLUE);
+        DrawRectangle(1216,5,32,32,BLUE);
+        DrawRectangle(1216,45,32,32,BLUE);
+        
+        
     
     
 
@@ -258,16 +210,42 @@ Sound breakBlock = LoadSound("res/sfx/sounds/random/break.ogg");
     
 
     //Draw Player
-    DrawTexture(playerTexture,dream.x,dream.y,WHITE);
+    //DrawTexture(playerTexture,dream.x,dream.y,WHITE);
 
     EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
+        
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
-    return 0;
+        //write level data to txt
+        ofstream myfile ("level.txt");
+        if (myfile.is_open())
+        {
+        for (int i = 0; i <40; i++)
+                {
+                        myfile << "{" ;
+                        for (int d = 0; d < 20; d++)
+                        {
+                                if (d < 20)
+                                myfile << levelData[i][d] << ",";
+                                else
+                                myfile << levelData[i][d];
+                        }
+                        if (i < 39)
+                        {
+                                myfile << "},";
+                        }
+                        else
+                        {
+                                myfile << "}";
+                        }
+                }
+        myfile.close();
+        }
+        else cout << "Unable to open file";
+        return 0;
 }
